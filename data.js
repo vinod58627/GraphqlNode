9a08f79a183a517057396cd971db26be72c6e38c
vinod-herb-ui <vinod.b@cfss.in>
2024-05-25 22:37:58 +0530

    push code

---
+++ data.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+let vin = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data received:', data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+console.log(vin)
